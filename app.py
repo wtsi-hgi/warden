15fdee9dc9d40e2d8e8a7882e675155cc5722e9a
Filip Makosza <fm12@sanger.ac.uk>
2020-02-18 14:43:15 +0000

    Added reactivity to instance launch and destruction

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import json
 import urllib.request
+import datetime
 
 import flask
 
 app = flask.Flask(__name__)
+
+# timestamp of when Warden last checked Arboretum's modification time
+LAST_CHECKED = datetime.datetime.now()
 
 @app.route('/')
 def index():
@@ -26,3 +30,19 @@ def destroyInstance(group):
         .format(group))
 
     return 'OK'
+
+@app.route('/update')
+def getGroupTable():
+    req = urllib.request.urlopen("http://localhost:8000/lastmodified")
+
+    result = json.loads(req.read())
+    last_modified = datetime.datetime.strptime(result, "%Y-%m-%d %H:%M:%S")
+
+    global LAST_CHECKED
+    if last_modified > LAST_CHECKED:
+        req = urllib.request.urlopen("http://localhost:8000/groups")
+        groups = json.loads(req.read())
+        LAST_CHECKED = datetime.datetime.now()
+        return groups
+    else:
+        return '"OK"'
--- static/filter.js
+++ static/filter.js
@@ -19,9 +19,74 @@ function filter() {
 }
 
 function startInstance(group) {
-    fetch('/create/'.concat('', group))
+    fetch('/create/'.concat('', group));
 }
 
 function destroyInstance(group) {
-    fetch('/destroy/'.concat('', group))
+    fetch('/destroy/'.concat('', group));
 }
+
+function modifyUpRowContents(groups, row) {
+    row.cells[1].innerHTML = "Estimated build time: " + groups['build_time']
+    + "<br>Status: " + groups['status'];
+
+    let button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.setAttribute('onclick', "destroyInstance(\""
+    + groups['group_name'] + "\")");
+    button.textContent = "Destroy";
+
+    let text = document.createElement('div');
+    text.innerHTML = "Prune time: " + groups['prune_time'] + "<br>Created: "
+    + groups['creation_time'];
+
+    row.cells[2].textContent = "";
+    row.cells[2].appendChild(button);
+    row.cells[2].appendChild(text);
+}
+
+function modifyDownRowContents(groups, row) {
+    row.cells[1].innerHTML = "Estimated build time: " + groups['build_time']
+    + "<br>Status: " + groups['status'];
+
+    let button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.setAttribute('onclick', "startInstance(\""
+    + groups['group_name'] + "\")");
+    button.textContent = "Launch";
+
+    row.cells[2].textContent = "";
+    row.cells[2].appendChild(button);
+}
+
+function updateGroupTable() {
+    fetch('/update')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(result) {
+            let groups = result;
+            let table = document.getElementById("groupTable");
+            let rows = table.rows;
+
+            if (groups === "OK") {
+                // database hasn't changed, no need to update
+                return false;
+            }
+
+            for (let i = 0; i < rows.length; i++) {
+                let name_cell = rows[i].cells[0];
+                let name = name_cell.textContent;
+
+                if (Object.keys(groups).includes(name)) {
+                    if (groups[name]["status"] === "up") {
+                        modifyUpRowContents(groups[name], rows[i]);
+                    } else if (groups[name]["status"] === "down") {
+                        modifyDownRowContents(groups[name], rows[i]);
+                    }
+                }
+            }
+        });
+}
+
+setInterval(updateGroupTable, 1000);
